74aac3b109ebeca191afbbdf65e35da7bda2d551
Nolita Rego <48682340+nolita26@users.noreply.github.com>
2020-07-03 14:55:04 +0530

    Delete main.dart

--- main.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:ftvapp/intropages/intro_layout.dart';
-
-void main() => runApp(MaterialApp(
-  debugShowCheckedModeBanner: false,
-  home: IntroLayout(),
-));
