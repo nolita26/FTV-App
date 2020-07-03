7d61c995b20a4e8df9377ba1d27325c8a03c43ed
Nolita Rego <48682340+nolita26@users.noreply.github.com>
2020-07-03 13:08:17 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:ftvapp/pages/homepage.dart';
+import 'package:ftvapp/pages/home_page.dart';
 
 void main() => runApp(MaterialApp(
   debugShowCheckedModeBanner: false,
