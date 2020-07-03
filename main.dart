3e877bdf7abb5f4193d7a9f44810199f4ba10938
Nolita Rego <48682340+nolita26@users.noreply.github.com>
2020-07-03 14:51:58 +0530

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:ftvapp/intropages/intro_layout.dart';
+
+void main() => runApp(MaterialApp(
+  debugShowCheckedModeBanner: false,
+  home: IntroLayout(),
+));
