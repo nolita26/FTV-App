eb5eb4f77c91e1e211c679087f0f170a57abf321
Nolita Rego <48682340+nolita26@users.noreply.github.com>
2020-07-03 13:07:51 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:ftvapp/intropages/intro_layout.dart';
+import 'package:ftvapp/pages/homepage.dart';
 
 void main() => runApp(MaterialApp(
   debugShowCheckedModeBanner: false,
-  home: IntroLayout(),
+  home: HomePage(),
 ));
