06381a029e707a5a1ff51a9db79289a8d564dfad
Nolita Rego <48682340+nolita26@users.noreply.github.com>
2020-07-06 19:44:19 +0530

    Add files via upload

--- lib/source/video_player.dart
+++ lib/source/video_player.dart
@@ -1,22 +1,271 @@
+import 'package:flutter/cupertino.dart';
+import 'package:ftvapp/pages/detailscreen.dart';
 import 'package:video_player/video_player.dart';
 import 'package:flutter/material.dart';
 import 'package:ftvapp/source/chewie_list_item.dart';
+import 'package:ftvapp/theme/color/light_color.dart';
 
-class VideoPlay extends StatelessWidget {
+class VideoPlay extends StatefulWidget {
+  @override
+  _VideoPlayState createState() => _VideoPlayState();
+}
+
+class _VideoPlayState extends State<VideoPlay> {
+
+//  RecommendedPage({Key key}) : super(key: key);
+
+  double width;
+
+  Widget _header(BuildContext context) {
+    var width = MediaQuery.of(context).size.width;
+    return ClipRRect(
+      borderRadius: BorderRadius.only(
+          bottomLeft: Radius.circular(50), bottomRight: Radius.circular(50)),
+      child: Container(
+          height: 90,
+          width: width,
+          decoration: BoxDecoration(
+            color: LightColor.orange,
+          ),
+          child: Stack(
+            fit: StackFit.expand,
+            alignment: Alignment.center,
+            children: <Widget>[
+              Positioned(
+                  top: 10,
+                  right: -120,
+                  child: _circularContainer(300, LightColor.lightOrange2)),
+              Positioned(
+                  top: -60,
+                  left: -65,
+                  child: _circularContainer(width * .5, LightColor.darkOrange)),
+              Positioned(
+                  top: -230,
+                  right: -30,
+                  child: _circularContainer(width * .7, Colors.transparent,
+                      borderColor: Colors.white38)),
+              Positioned(
+                  top: 50,
+                  left: 0,
+                  child: Container(
+                      width: width,
+                      padding: EdgeInsets.symmetric(horizontal: 20),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          GestureDetector(
+                            onTap: () {
+                              Navigator.push(context, MaterialPageRoute(builder: (_) => DetailScreen()));
+                            },
+                            child: Icon(
+                              Icons.arrow_back_ios,
+                            ),
+                          ),
+                        ],
+                      ),
+                  )),
+            ],
+          )),
+    );
+  }
+
+  Widget _circularContainer(double height, Color color,
+      {Color borderColor = Colors.transparent, double borderWidth = 2}) {
+    return Container(
+      height: height,
+      width: height,
+      decoration: BoxDecoration(
+        shape: BoxShape.circle,
+        color: color,
+        border: Border.all(color: borderColor, width: borderWidth),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
+    width = MediaQuery.of(context).size.width;
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Video Player"),
+      body: SingleChildScrollView(
+        child: Container(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              _header(context),
+              Container(
+                height: 260,
+                child: ListView(
+                  children: <Widget>[
+                    ChewieListItem(
+                      videoPlayerController: VideoPlayerController.asset("videos/video1.mp4"),
+                      looping: true,
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(height: 20),
+              Padding(
+                padding: const EdgeInsets.only(left: 20.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      "Business Foundation",
+                      style: TextStyle(
+                        fontSize: 25,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Stack(
+                  children: <Widget>[
+                    Padding(
+                      padding: const EdgeInsets.all(30),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+//                          Text("Course Content",
+//                            style: TextStyle(
+//                              fontSize: 24,
+//                              color: Color(0xFF0D1333),
+//                              fontWeight: FontWeight.bold,
+//                            ),
+//                          ),
+//                          SizedBox(height: 20),
+                          CourseContent(
+                            number: "01",
+                            duration: 5.35,
+                            title: "Welcome to the Course",
+                            isDone: true,
+                          ),
+                          CourseContent(
+                            number: '02',
+                            duration: 19.04,
+                            title: "Beauty Care - Intro",
+                            isDone: true,
+                          ),
+                          CourseContent(
+                            number: '03',
+                            duration: 15.35,
+                            title: "Beauty Care Process",
+                            isDone: false,
+                          ),
+                          CourseContent(
+                            number: '04',
+                            duration: 5.35,
+                            title: "Customer Perspective",
+                            isDone: false,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ]),
+            ],
+          ),
+        ),
       ),
-      body: ListView(
+    );
+  }
+}
+
+class CourseContent extends StatelessWidget {
+  final String number;
+  final double duration;
+  final String title;
+  final bool isDone;
+  const CourseContent({
+    Key key,
+    this.number,
+    this.duration,
+    this.title,
+    this.isDone = false,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 30),
+      child: Row(
         children: <Widget>[
-          ChewieListItem(
-            videoPlayerController: VideoPlayerController.asset("videos/video1.mp4"),
-            looping: true,
+          Text(
+            number,
+            style: TextStyle(
+              fontSize: 28,
+              color: Color(0xFF0D1333),
+              fontWeight: FontWeight.bold,
+            ).copyWith(
+              color: Color(0xFF0D1333).withOpacity(.15),
+              fontSize: 32,
+            ),
+          ),
+          SizedBox(width: 20),
+          RichText(
+            text: TextSpan(
+              children: [
+                TextSpan(
+                  text: "$duration mins\n",
+                  style: TextStyle(
+                    color: Color(0xFF0D1333).withOpacity(.5),
+                    fontSize: 18,
+                  ),
+                ),
+                TextSpan(
+                  text: title,
+                  style: TextStyle(
+                    fontSize: 18,
+                    color: Color(0xFF0D1333),
+                    // fontWeight: FontWeight.bold,
+                  ).copyWith(
+                    fontWeight: FontWeight.w600,
+                    height: 1.5,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Spacer(),
+          Container(
+            margin: EdgeInsets.only(left: 20),
+            height: 40,
+            width: 40,
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              color: LightColor.orange.withOpacity(isDone ? 1 : .5),
+            ),
+            child: GestureDetector(
+              onTap: () {
+                Navigator.push(context, MaterialPageRoute(builder: (_) => VideoPlay()));
+              },
+              child: Icon(
+                Icons.play_arrow,
+                color: Colors.white,
+              ),
+            ),
           )
         ],
       ),
     );
   }
 }
+
+
+
+//class VideoPlay extends StatelessWidget {
+//  @override
+//  Widget build(BuildContext context) {
+//    return Scaffold(
+//      appBar: AppBar(
+//        title: Text("Video Player"),
+//      ),
+//      body: ListView(
+//        children: <Widget>[
+//          ChewieListItem(
+//            videoPlayerController: VideoPlayerController.asset("videos/video1.mp4"),
+//            looping: true,
+//          )
+//        ],
+//      ),
+//    );
+//  }
+//}
